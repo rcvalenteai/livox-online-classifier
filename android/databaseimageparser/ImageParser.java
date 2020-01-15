bd50dcc1ff6d45b68ab61a087400a228cbaf0a66
rcvalenteai <rcvalenteai@gmail.com>
2020-01-15 14:59:29 -0500

    java android version code

---
+++ android/databaseimageparser/ImageParser.java
@@ -0,0 +1,44 @@
+package databaseimageparser;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ImageParser {
+    // TODO: proper initialization of db
+    MySQLDB db = new MySQLDB();
+
+    private String notFound(String keyword) {
+        // TODO: convert logic
+        return "";
+    }
+
+    private String stemmed(String keyword) {
+        // TODO: convert logic
+        return "";
+    }
+
+    private String closest(String keyword) {
+        // TODO: convert logic
+        return "";
+    }
+
+    private String translate(String keyword) {
+        // TODO: convert logic
+        return "";
+    }
+
+    private List<String> searchTags(String keyword) {
+        // TODO: convert logic
+        return new LinkedList<String>();
+    }
+
+    private String searchImageWord(String keyword) {
+        // TODO: convert logic
+        return "";
+    }
+
+    public String getImage(String keyword) {
+        // TODO: convert logic
+        return "";
+    }
+}
---
+++ android/resources/tables.sql
@@ -0,0 +1,40 @@
+/*
+Images
+Primary_Key -> image_id
+ */
+ CREATE TABLE Images
+        (image_id VARCHAR(36),
+         location VARCHAR(256),
+         Constraint p_key_images Primary Key (image_id));
+
+/*
+Labels
+Primary Key -> image_id, label
+Foreign Key -> Images.image_id
+ */
+ CREATE TABLE Labels
+        (image_id VARCHAR(36),
+         label VARCHAR(32),
+         confidence REAL,
+         Constraint p_key_labels Primary Key (image_id, label),
+         Foreign Key (image_id) References Images(image_id));
+
+/*
+         Foreign Key (image_id) References Images(image_id)
+ */
+
+
+/*
+Tags
+Primary Key -> image_id, tag
+Foreign Key -> Images.image_id
+ */
+ CREATE TABLE Tags
+        (image_id VARCHAR(36),
+         tag VARCHAR(32),
+         Constraint p_key_tags Primary Key (image_id, tag),
+         Foreign Key (image_id) References Images(image_id));
+
+/*
+         Foreign Key (image_id) References Images(image_id)
+ */
