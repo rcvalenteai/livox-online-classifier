e8d81bb7c3b9d5320c9b41b0bbb373fda927d32b
rcvalenteai <rcvalenteai@gmail.com>
2020-01-16 17:44:24 -0500

    updated plot small change

--- evaluation/plots.py
+++ evaluation/plots.py
@@ -4,18 +4,19 @@ import evaluation.mturk_metadata as metadata
 
 def plot_word_frequency(filename):
     word_frequency = metadata.question_word_frequency(filename)
-    plot_dictionary_counter(word_frequency)
+    plot_dictionary_counter(word_frequency, "Question Word Frequency Count")
 
 
 def plot_ngram_frequency(filename):
     ngram_frequency = metadata.ngram_frequency(filename)
-    plot_dictionary_counter(ngram_frequency)
+    plot_dictionary_counter(ngram_frequency, "Ngram Frequency Count")
 
 
-def plot_dictionary_counter(dict_counter):
+def plot_dictionary_counter(dict_counter, title):
     dict_counter = {k: v for k, v in sorted(dict_counter.items(), key=lambda item: item[1], reverse=True)}
     plt.bar(range(len(dict_counter)), list(dict_counter.values()), align='edge')
     plt.xticks(range(len(dict_counter)), list(dict_counter.keys()))
+    plt.title(title)
     plt.show()
 
 
