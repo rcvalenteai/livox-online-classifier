38711549e8668fba85650d35001236ab647e05fe
rcvalenteai <rcvalenteai@gmail.com>
2020-01-28 20:28:59 -0500

    update parse phrases, NER package update, data augmentation, fixed summary evaluation bugs

---
+++ evaluation/data-cleaning/yes_no_cleaner.py
@@ -0,0 +1,32 @@
+import helpers.io
+import api.entity_phrase_parser.vocab_generator as vocab_gen
+import json
+
+
+def create_yes_no_argus_csv(filename="./data/argus_yes_no_train.csv"):
+    argus = helpers.io.load_csv(filename)[1:]
+    yes_nos = dict()
+    final_yes_nos = list()
+    for example in argus:
+        cleaned = example[0].replace(" ' ", "").replace(" ?", "").lower()
+        yes_nos[cleaned] = 1
+    for question, value in yes_nos.items():
+        row = [question, [], "", 0]
+        final_yes_nos.append(row)
+    helpers.io.write_list_csv("cleaned", "argus.csv", final_yes_nos[:500])
+
+
+def create_boolq_yes_no_csv(filename="./data/boolq_yes_no.jsonl"):
+    result = list()
+    with open(filename, "r", encoding='utf-8') as file:
+        print(file)
+        for line in file:
+            result.append(json.loads(line)['question'])
+    final_results = list()
+    for res in result:
+        row = [res, [], "", 0]
+        final_results.append(row)
+    helpers.io.write_list_csv("cleaned", "boolq.csv", final_results[:500])
+
+
+create_boolq_yes_no_csv()
