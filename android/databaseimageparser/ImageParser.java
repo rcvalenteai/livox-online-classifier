e84ca0abe5e18c0caef0a7295c2b4e60197c417a
rcvalenteai <rcvalenteai@gmail.com>
2020-02-07 09:21:13 -0500

    updated image parsing

--- android/databaseimageparser/ImageParser.java
+++ android/databaseimageparser/ImageParser.java
@@ -2,10 +2,14 @@ package databaseimageparser;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Dictionary;
+import android.database.sqlite.SQLiteDatabase;
 
 public class ImageParser {
     // TODO: proper initialization of db
-    MySQLDB db = new MySQLDB();
+    MySQLDB db_open = new MySQLDB();
+    SQLiteDatabase db = db_open.getReadableDatabase();
+
 
     private String notFound(String keyword) {
         String path = "symbol00071621.png";
@@ -30,32 +34,80 @@ public class ImageParser {
     }
 
     private String stemmed(String keyword) {
-        // TODO: convert logic
+        // TODO: convert logic (non-essential)
+        throw new Exception();
         return "";
     }
 
     private String closest(String keyword) {
-        // TODO: convert logic
+        // TODO: convert logic (non-essential)
+        throw new Exception();
         return "";
     }
 
     private String translate(String keyword) {
-        // TODO: convert logic
+        throw new Exception();
+        // TODO: convert logic (non-essential)
         return "";
     }
 
     private List<String> searchTags(String keyword) {
-        // TODO: convert logic
+        // TODO: convert logic (non-essential)
         return new LinkedList<String>();
     }
 
+
+    private Dictionary searchLabels(String keyword) {
+        String stmt = "SELECT image_id, confidence FROM Labels WHERE label = ?s";
+        Cursor labels_response = db.rawQuery(stmt, [keyword]);
+        labels_response.moveToFirst();
+        Dictionary labels = new Hashtable();
+        while (!labels_response.isAfterLast())
+            labels.put(labels_response.getString("image_id"), labels.getDouble("confidence"));
+        }
+        return labels;
+    }
+
+    private String searchImages(String image_id) {
+        String stmt = "SELECT location FROM Images WHERE image_id = ?s";
+        Cursor images_response = db.rawQuery(stmt, [image_id]);
+        images_response.moveToFirst();
+        String path = images_response.getString("location");
+        return path;
+    }
+
+    private String findMaxImageID(Dictionary labels) {
+    if (labels.isEmpty()) {
+        throw new Exception();
+    }
+    // TODO: convert logic
+    return ""
+    }
+
     private String searchImageWord(String keyword) {
-        // TODO: convert logic
-        return "";
+        Dictionary labels = searchLabels(keyword);
+        LinkedList tags = searchTags(keyword);
+        for(String tag: tags) {
+            if (labels.get(tag) != null) {
+                labels.put(tag, labels.get(tag) + 0.85);
+            }
+            else {
+                labels.put(tag, 0.85);
+            }
+        }
+
+        String path = searchImages(findMaxImageID(labels);
+        return path;
     }
 
     public String getImage(String keyword) {
-        // TODO: convert logic
-        return "";
+        String path = "";
+        try {
+            path = searchImageWord(keyword);
+        } catch (Exception e5) {
+            path = notFound(keyword);
+        }
+        path = "https://storage.googleapis.com/livox-images/full/" + path;
+        return path;
     }
 }
